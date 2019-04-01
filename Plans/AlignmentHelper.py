d45b31f4503689b63303d74d0736bdc880215a24
Till Stensitzki <mail.till@gmx.de>
2019-04-01 11:18:38 +0200

    Make sure spec controls are working.

--- Plans/AlignmentHelper.py
+++ Plans/AlignmentHelper.py
@@ -12,10 +12,53 @@ class AlignmentHelper(QWidget):
         super().__init__(**kwargs)
         self.times = []
         self.time_max = 20
+        self.t0 = time.time()
         self.controller = controller
         cam1_obs = (controller.cam.last_read)
 
-        self.left_plots = qh.ObserverPlot(parent=self, x=self.times)
+
+        self.graph_layouter = pg.GraphicsLayoutWidget()
+        self.setLayout(qh.hlay([self.graph_layouter]))
+        self.plots = {}
+        self.amp_lines = {}
+        self.std_lines = {}
+
+        for cam in controller.cam_list:
+            amp_plot = self.graph_layouter.addPlot()
+            std_plot = self.graph_layouter.addPlot()
+            self.graph_layouter.nextRow()
+            for l in range(cam.lines):
+                self.amp_lines[(cam, l)] = amp_plot.plot()
+                self.std_lines[(cam, l)] = std_plot.plot()
+
+            cam.sigReadCompleted.connect(self.update_plots)
+
+    def update_plots(self):
+        t = time.time()
+        self.times.append(t-self.t0)
+        if t-self.times[0] > 20:
+            self.times.pop(0)
+
+        for (cam, line), p in self.amp_lines.items():
+            p.setData(cam.last_ )
+
+
+if __name__ == '__main__':
+    c = Controller()
+
+    app = pg.mkQApp()
+    a = AlignmentHelper(c)
+
+    def looper():
+        while True:
+            c.loop()
+
+    import threading
+    t = threading.Thread(target=looper)
+    t.start()
+    app.aboutToQuit.conenct(t.s)
+    a.show()
+    app.exec_()
 
 
 
