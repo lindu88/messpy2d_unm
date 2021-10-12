3b0e1a834d11f2bf52ae5419ff48bbc47eb67ccb
tillsten <mail.till@gmx.de>
2021-10-12 12:30:19 +0200

    more cam work

--- Instruments/dac_px/pxdac.py
+++ Instruments/dac_px/pxdac.py
@@ -1,9 +1,10 @@
+from attr import attr
 from nicelib import load_lib, NiceLib, Sig, NiceObject, RetHandler, ret_ignore, ret_return
 import logging
 
 @RetHandler(num_retvals=0)
 def ret_errcode(retval, funcargs, niceobj):
-    
+
     if retval != 0:
         raise ValueError(PXDAC.GetErrorMessXD48(retval, 1,
                                                 niceobj._handles[0]))
@@ -63,24 +64,24 @@ class PXDAC(NiceLib):
 
         def get_output_voltage(self):
             vals = (self.GetOutputVoltageCh1XD48(1), self.GetOutputVoltageCh2XD48(1),
-                    self.GetOutputVoltageCh3XD48(1), self.GetOutputVoltageCh4XD48(1))                
+                    self.GetOutputVoltageCh3XD48(1), self.GetOutputVoltageCh4XD48(1))
             return list(map(lambda i: PXDAC.GetOutputVoltageRangeVoltsXD48(i, self._handles[0]), vals))
-        
+
         def set_output_voltage(self, ch1=None, ch2=None, ch3=None, ch4=None):
             def check(x):
                 if 0 <= x <= 1023:
                     return x
                 else:
                     raise ValueError("Voltage settings have to be between 0 and 1023")
-            if ch1 is not None:                
+            if ch1 is not None:
                 check(ch1)
                 self.SetOutputVoltageCh1XD48(ch1)
-            if ch2 is not None:                
+            if ch2 is not None:
                 check(ch2)
                 self.SetOutputVoltageCh2XD48(ch2)
-            if ch3 is not None:                
+            if ch3 is not None:
                 self.SetOutputVoltageCh3XD48(check(ch3))
-            if ch4 is not None:                
+            if ch4 is not None:
                 self.SetOutputVoltageCh4XD48(check(ch4))
 
 import sys
@@ -90,6 +91,7 @@ handler = logging.StreamHandler(sys.stdout)
 handler.filter('pxdac')
 log.addHandler(handler)
 
+@attr()
 class AOM:
     def __init__(self):
         dac = PXDAC.DAC(1)
@@ -97,18 +99,38 @@ class AOM:
         log.info("Size: %s ", dac.GetDacSampleSizeXD48(1))
         log.info("Format: %s", dac.GetDacSampleFormatXD48(1))
         log.info("Output Voltage %s", dac.get_output_voltage())
-        dac.set_output_voltage(ch1=1000, ch2=1000)
+        dac.set_output_voltage(ch1=200, ch2=1000)
         log.info("Output Voltage %s", dac.get_output_voltage())
         dac.SetExternalTriggerEnableXD48(1)
         log.info("ExtTigger %s", dac.GetExternalTriggerEnableXD48(1))
         dac.SetTriggerModeXD48(0)
         dac.SetActiveChannelMaskXD48(0x1 | 0x2)
         self.dac = dac
+        self.amp_fac = 1
+        self.calib = None
+        self.nu = None
 
-    def amplitude(self, i: int):
+    def voltage(self, i: int):
         if (0<i<1023):
             raise ValueError('i')
         self.dac.set_output_voltage(ch1=i)
+
+    def set_calib(self, p):
+
+
+    def set_wave_amp(self, amp):
+        if not (0 < amp < 1):
+            raise ValueError('Amptiltude has to be between 0 and 1')
+        V = 1.4*amp
+        if V  > 0.4:
+            k = int((V - 0.4)*1023)
+            self.voltage(k)
+            self.amp_fac = 1
+        else:
+            self.voltage(0)
+            f = V/0.4
+            self.amp_fac = f
+
 
     def load_mask(self, mask):
         self.mask = mask
@@ -123,37 +145,34 @@ class AOM:
     def end_playback(self):
         self.dac.EndRamPlaybackXD48()
 
-    def make_calib_mask(self):
+    def make_calib_mask(self, width=150, seperation=350, single=6000):
         import numpy as np
         #mask1 = np.ones(4096 * 3, dtype=np.int16)
         b14max = (1 << 15) - 1
-        fc = (1/16)
 
-        fn = ()
+
         mask2 = np.cos(np.arange(4096*3)/16*2*np.pi)*b14max
         mask21 = mask2.copy()
-        for i in range(0, mask2.size, 500):
+        total = width + seperation
+        for i in range(0, mask2.size, total):
             mask21[i+150:i+500] = 0
-        i = 6000
+        i = single
         mask22 = np.zeros_like(mask21)
-        mask22[i:i+150] = mask2[i:i+150]
-        mask2 = np.hstack((mask21, mask22))
-        mask1 = np.ones((4096*3), dtype=np.int16)*b14max
-        mask1 = np.hstack((mask1, 0*mask1))
 
-        #mask2 = np.ones(4096 * 3, dtype=np.int16)
-        #mask2 = np.tile(mask2, 1000).reshape(mask2.size, 1000)
-        # mask2.ravel("f")
-        #amps = np.linspace(-b14max, b14max, 1000)
-        # amps[::2] = 0
-        #mask1 = np.tile(mask1, amps.size).reshape(mask1.size, amps.size)
+        mask22[i:i+150] = mask2[i:i+150]
+        # Three frames: train, single and full
+        mask2 = np.hstack((mask21, mask22, mask2))
+
+        # Sync mask on channel 2: b14max 0 0 0 .....
+        mask1 = np.ones((4096*3), dtype=np.int16)*b14max
+        mask1 = np.hstack((mask1, 0*mask1, 0*mask1))
         mask2 = mask2.astype('int')
-        #mask1 = mask1.flatten('f')
-        mask = np.zeros(mask2.size * 2, dtype=np.int16)
+
+        # Interleave
+        mask = np.zeros(mask2.size * 3, dtype=np.int16)
         mask[1::2] = mask1
         mask[::2] = mask2
-        return mask
+        return self.amp_fac*mask
 
 if __name__ == '__main__':
-    A = AOM()
-    A.load_mask(A.make_calib_mask())
+    A = AOM
--- Plans/AsyncPlan.py
+++ Plans/AsyncPlan.py
@@ -1,7 +1,7 @@
 import asyncio
 import asyncio as aio
 import attr
-#from PyQt5.QtWidgets import QWidget
+#from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QHBoxLayout
 import os, sys
 
 os.environ['QT_API'] = 'PyQt5'
@@ -12,16 +12,19 @@ from qasync import QEventLoop, asyncClose, asyncSlot
 from qtpy.QtWidgets import *
 from qtpy.QtCore import QObject, Signal
 import pyqtgraph as pg
+from pyqtgraph.parametertree import Parameter, ParameterItem, ParameterTree
 import numpy as np
 
+
 @attr.s(auto_attribs=True, cmp=False)
-class FocusScan(QObject):
+class CalibPlan(QObject):
     cam: ICam
     move_func: Callable
     points: List[float]
     amps: List[List[float]] = attr.Factory(list)
     start_pos: Tuple[float, float] = 0
-
+    check_zero_order: bool = True
+    channel: int = 67
     sigStepDone = Signal()
     sigPlanDone = Signal()
 
@@ -29,6 +32,11 @@ class FocusScan(QObject):
         super().__init__()
 
     async def step(self):
+        loop = asyncio.get_running_loop()
+        if self.check_zero_order:
+            self.cam.set_wavelength(0, 10)  #
+            reading = await loop.run_in_executor(None, self.cam.make_reading)
+
         await self.pre_scan()
         for p in self.points:
             await self.read_point(p)
@@ -54,22 +62,56 @@ class FocusScan(QObject):
 
 
 
+
 class FocusScanView(QWidget):
-    def __init__(self, focus_scan: FocusScan, *args, **kwargs):
-        super().__init__( *args, **kwargs)
+    def __init__(self, focus_scan: CalibPlan, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.info_label = QLabel('BLa')
         self.focus_scan = focus_scan
         self.focus_scan.sigStepDone.connect(self.update_view)
+
         self.setLayout(QHBoxLayout())
         self.start_button = QPushButton('start')
         self.layout().addWidget(self.info_label)
         self.layout().addWidget(self.start_button)
+
+
+        self.children = [
+            dict(name='start_wl', type='float', value=4000, step=500),
+            dict(name='end_wl', type='float',  value=6000, step=500),
+            dict(name='step', type='float',  value=10, step=2),
+        ]
+        param = Parameter.create(name='Calibration Scan',
+                                type='group',
+                                children=self.children)
+        for c in param.children():
+            c.setDefault(c.value())
+
+        self.children2 = [
+            dict(name='Height', type='float', value=100000),
+            dict(name='Distance', type='float', value=5),
+
+        ]
+        param2 = Parameter.create(name='Calibration Scan',
+                            type='group',
+                            children=self.children2)
+        self.params : Parameter = param
+        self.pt = ParameterTree(self.params)
+        self.layout().addWidget(self.pt)
         self.start_button.clicked.connect(self.start)
         self.plot = pg.PlotWidget(self)
         self.layout().addWidget(self.plot)
         self.focus_scan.sigPlanDone.connect(self.analyse)
+        self.focus_scan.sigPlanDone.connect(
+            lambda: self.start_button.setEnabled(True))
 
     def start(self):
+        s = self.params.saveState()
+        start, stop, step = self.params['start_wl'].value, self.params['end_wl'].value, self.params['step'].value
+        self.focus_scan.points = np.arange(start, stop, step)
+        self.params.setReadonly(True)
+
+        self.start_button.setDisabled(True)
         loop = aio.get_event_loop()
         loop.create_task(self.focus_scan.step())
 
@@ -83,19 +125,19 @@ class FocusScanView(QWidget):
         x = plan.points[:n]
         self.plot.plotItem.plot(x, np.array(plan.amps)[:, 1], pen='r')
         self.plot.plotItem.plot(x, np.array(plan.amps)[:, 0], pen='g')
+        self.plot.plotItem.plot(x, np.array(plan.amps)[:, 0], pen='y')
 
     def analyse(self):
         plan = self.focus_scan
         x = np.array(plan.points)
         y1 = np.array(plan.amps)[:, 1]
         y2 = np.array(plan.amps)[:, 0]
-        np.save('calib.npy' ,np.column_stack((x, y1, y2)))
+        np.save('calib.npy', np.column_stack((x, y1, y2)))
         from scipy.signal import find_peaks
         p1, _ = find_peaks(y1, height=4000, distance=5)
         p2, _ = find_peaks(y1, height=4000, distance=5)
         self.plot.plotItem.plot(x[p1], y1[p1])
         self.plot.plotItem.plot(x[p2], y2[p2])
-
 
 
 if __name__ == '__main__':
@@ -107,21 +149,10 @@ if __name__ == '__main__':
     aio.set_event_loop(loop)
     cam = _ircam
     cam.set_shots(50)
-    fs = FocusScan(cam=cam, move_func=cam.set_wavelength,
+    fs = FocusScan(cam=cam,
+                   move_func=cam.set_wavelength,
                    points=np.arange(5500, 6500, 5))
     fv = FocusScanView(fs)
     fv.show()
 
-
-
-
-
-    loop.run_forever()
-
-
-
-
-
-
-
-
+    loop.run
