9f0f74111154cbb0e9d1181bf3bec87211e7f894
tillsten <mail.till@gmx.de>
2021-10-15 00:36:38 +0200

    comment out two d

--- Plans/__init__.py
+++ Plans/__init__.py
@@ -1,7 +1,7 @@
 from .PumpProbe import PumpProbePlan
 from .PumpProbeViewer import PumpProbeViewer, PumpProbeStarter
-from .TwoDPlan import TwoDimMoving
-from .TwoDPlanViewer import TwoDViewer, TwoDStarter
+#from .TwoDPlan import TwoDimMoving
+#from .TwoDPlanViewer import TwoDViewer, TwoDStarter
 from .ScanSpectrum import ScanSpectrum
 from .ScanSpectrumView import ScanSpectrumView, ScanSpectrumStarter
 from .AlignmentHelper import AlignmentHelper
