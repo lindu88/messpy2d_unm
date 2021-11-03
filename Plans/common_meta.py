a9761dcdc57aee58f62e715e6f6f0231859c4c64
Dr. rer. n Till Stensitzki <till.stensitzki@uni-potsdam.de>
2021-11-03 16:56:57 +0100

    rename sample parameters

--- Plans/common_meta.py
+++ Plans/common_meta.py
@@ -1,20 +1,22 @@
 from Config import config
 import attr, time
+from typing import Optional
 
-samp = {'name': 'Sample', 'type': 'group', 'children': [
+sample_parameters = {'name': 'Sample', 'type': 'group', 'children': [
     dict(name='Sample', type='str', value=''),
     dict(name='Solvent', type='list', values=config.list_of_solvents),
     dict(name='Excitation', type='str'),
     dict(name='Thickness', type='str'),
     dict(name='Annotations', type='str'),
-    dict(name='Users:', type='str'),]}
-    
-    
+    dict(name='Users:', type='str'),
+]}
+
+
 
 @attr.s(auto_attribs=True)
 class Plan:
-    name: str
-    meta: dict 
+    name: str = None
+    meta: Optional[dict] = None
     status: str = ''
     creation_time: float = attr.Factory(time.time)
 
