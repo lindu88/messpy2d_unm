90377d94b13d6f03c89c09c879c4d2975b3f277d
Till Stensitzki <mail.till@gmx.de>
2023-01-12 13:45:55 +0100

    Start improving 2d view

--- Plans/AOMTwoDView.py
+++ Plans/AOMTwoDView.py
@@ -14,6 +14,36 @@ from .PlanBase import sample_parameters
 from typing import Callable
 import qasync
 
+
+@attr.dataclass
+class TwoDimPlotter(PlotWidget):
+    plan: AOMTwoDPlan
+    img_2dspec: ImageItem = attr.ib(factory=ImageItem)
+    hist_item: HistogramLUTItem = attr.ib(factory=HistogramLUTItem)
+
+    probe_freq: np.ndarray = attr.ib()
+    pump_freqs: np.ndarray = attr.ib()
+
+    @probe_freq.default
+    def _read_freqs(self):
+        self.plan.probe_freqs
+
+    @pump_freqs.default
+    def _read_freqs(self):
+        self.plan.pump_freqs
+
+    def __attrs_post_init__(self):
+        self.img_2dspec.setImage(np.zeros((128, self.plan.t1.size)))
+        self.img_2dspec.setRect((self.probe_freq.max(), self.pump_freqs[-1], -self.probe_freq.ptp(),
+                                 self.pump_freqs[0] - self.pump_freqs[-1]))
+        self.addItem(self.img_2dspec)
+        self.hist_item.setImageItem(self.img_2dspec)
+        self.addItem(self.hist_item)
+
+    def update_image(self):
+        self.img_2dspec.setImage(self.plan.last_2d[::, ::-1])
+
+
 class AOMTwoDViewer(GraphicsLayoutWidget):
     def __init__(self, plan: AOMTwoDPlan, parent=None):
         super().__init__(parent=parent)
@@ -46,11 +76,13 @@ class AOMTwoDViewer(GraphicsLayoutWidget):
         self.spec_img = ImageItem()
         rect = (self.probe_freq.max(), self.pump_freqs[-1], -self.probe_freq.ptp(),
                 self.pump_freqs[0]-self.pump_freqs[-1])
-        self.spec_img.setImage(np.zeros((128, plan.pump_freqs.size)), rect=rect)
+        self.spec_img.setImage(
+            np.zeros((128, plan.pump_freqs.size)), rect=rect)
 
         pw.addItem(self.spec_img)
         self.spec_line = InfiniteLine(pos=self.pump_freqs[self.pump_freqs.size//2], angle=0,
-                                      bounds=(self.pump_freqs.min(), self.pump_freqs.max()),
+                                      bounds=(self.pump_freqs.min(),
+                                              self.pump_freqs.max()),
                                       movable=True)
         pw.addItem(self.spec_line)
         hist = HistogramLUTItem()
@@ -59,17 +91,18 @@ class AOMTwoDViewer(GraphicsLayoutWidget):
 
         self.addItem(hist)
         self.ci.nextRow()
-        self.trans_plot = self.ci.addPlot(colspan=2)
-        self.trans_plot.setLabels(bottom="Time", left='Signal')
+
         self.spec_plot = self.ci.addPlot(colspan=2)
         self.spec_plot.setLabels(bottom="Probe Freq", left='Signal')
         self.spec_cut_line = self.spec_plot.plot()
         self.spec_mean_line = self.spec_plot.plot()
+        self.spec_current_line = self.spec_plot.plot()
         self.ci.nextRow()
         self.info_label = self.ci.addLabel("Hallo", colspan=4)
-        
+
         self.update_plots()
         self.spec_line.sigPositionChanged.connect(self.update_spec_lines)
+        self.spec_line.sig
         self.plan.sigStepDone.connect(self.update_data)
         self.plan.sigStepDone.connect(self.update_plots)
         self.plan.sigStepDone.connect(self.update_label)
@@ -82,36 +115,11 @@ class AOMTwoDViewer(GraphicsLayoutWidget):
             self.ifr_img.setImage(self.plan.last_ir, autoLevels=al)
             self.spec_img.setImage(self.plan.last_2d[::, ::-1], autoLevels=al)
 
-    def ifr_clicked(self, ev):
-        x, y = ev.pos()
-        if len(self.ifr_free_colors) == 0:
-            return
-        _int_color = self.ifr_free_colors.pop(0)
-        line = self.spec_image_view.addLine(x=self.probe_freq[round(x)], movable=True,
-                                            bounds=(self.probe_freq.min(), self.probe_freq.max()),
-                                            pen=mkPen(_int_color, width=1))
-        line._int_color = _int_color
-        cur_line = 'Probe2'
-        #self.ifr_lines[line] = self.trans_plot.plot(self.plan.t1, self.plan.disp_arrays[cur_line][1][round(x), :],
-        #                                            pen=line.pen)
-
-        def update(line: InfiniteLine):
-            idx = np.argmin(abs(self.probe_freq - line.pos()[0]))
-            self.ifr_lines[line].setData(self.plan.t1, self.plan.last_ir[round(idx), :])
-
-        def delete(line: InfiniteLine, ev):
-            ev.accept()
-            trans_line = self.ifr_lines.pop(line)
-            self.trans_plot.removeItem(trans_line)
-            self.spec_image_view.removeItem(line)
-            self.ifr_free_colors.append(line._int_color)
-
-        line.sigClicked.connect(delete)
-        line.sigPositionChanged.connect(update)
-
     def update_plots(self):
+        # self.spec_current_line.setData(self.probe_freq, self.plan.)
         if self.plan.last_2d is not None:
-            self.spec_mean_line.setData(self.probe_freq, self.plan.last_2d.mean(1))
+            self.spec_mean_line.setData(
+                self.probe_freq, self.plan.last_2d.mean(1))
             for line in self.ifr_lines.keys():
                 line.sigPositionChanged.emit(line)  # Causes an update
 
@@ -149,12 +157,17 @@ class AOMTwoDStarter(PlanStartDialog):
 
         tmp = [{'name': 'Filename', 'type': 'str', 'value': 'temp'},
                {'name': 'Operator', 'type': 'str', 'value': 'Till'},
-               {'name': 't1 (+)', 'suffix': 'ps', 'type': 'float', 'value': -4},
-               {'name': 't1 (step)', 'suffix': 'ps', 'type': 'float', 'value': 0.1},
+               {'name': 't1 (+)', 'suffix': 'ps',
+                'type': 'float', 'value': -4},
+               {'name': 't1 (step)', 'suffix': 'ps',
+                'type': 'float', 'value': 0.1},
                {'name': 'Phase Cycles', 'type': 'list', 'values': [1, 2, 4]},
-               {'name': 'Rot. Frame', 'suffix': 'cm-1', 'type': 'int', 'value': 2000},
-               {'name': 'Mode', 'type': 'list', 'values': ['classic', 'bragg']},
-               {'name': 'AOM Amp.', 'type': 'float', 'value': 0.3, 'min': 0, 'max': 0.6},
+               {'name': 'Rot. Frame', 'suffix': 'cm-1',
+                   'type': 'int', 'value': 2000},
+               {'name': 'Mode', 'type': 'list',
+                   'values': ['classic', 'bragg']},
+               {'name': 'AOM Amp.', 'type': 'float',
+                   'value': 0.3, 'min': 0, 'max': 0.6},
                {'name': 'Repetitions', 'type': 'int', 'value': 1},
                DelayParameter(),
                {'name': 'Save Frames', 'type': 'bool', 'value': False},
@@ -163,7 +176,8 @@ class AOMTwoDStarter(PlanStartDialog):
 
         two_d = {'name': 'Exp. Settings', 'type': 'group', 'children': tmp}
         params = [sample_parameters, two_d]
-        self.paras = pt.Parameter.create(name='Pump Probe', type='group', children=params)
+        self.paras = pt.Parameter.create(
+            name='Pump Probe', type='group', children=params)
 
     def create_plan(self, controller: Controller):
         p = self.paras.child('Exp. Settings')
