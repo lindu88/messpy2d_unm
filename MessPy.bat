5c891497e6d8233bdd82d1f34907e4caaf7d23a3
Dr. rer. n Till Stensitzki <till.stensitzki@uni-potsdam.de>
2024-02-19 13:42:38 +0100

    Add bat file for startup with a icon

---
+++ MessPy.bat
@@ -0,0 +1,3 @@
+call conda activate imaq2
+cd "C:\Users\MUELLER-WERKMEISTER\Downloads\download\messpy2d - Copy\"
+C:\Users\MUELLER-WERKMEISTER\anaconda3\envs\imaq2\python.exe "C:/Users/MUELLER-WERKMEISTER/Downloads/download/messpy2d - Copy/start.py"
