13cfbe5b81e33219bf247004db98dd229d0c02a9
Dr. rer. n Till Stensitzki <till.stensitzki@uni-potsdam.de>
2024-02-27 10:16:26 +0100

    Make it run on 3.11

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import os
-import PySide6
-print(os.environ['QT_API'])
-os.environ['QT_API'] = 'pyside6'
+#import PySide6
+#print(os.environ['QT_API'])
+#os.environ['QT_API'] = 'pyside6'
 import qasync
 from MessPy.MessPy2D import start_app
 
